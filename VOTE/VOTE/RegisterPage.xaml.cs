8c2f8c57dce4020480ae3ae023f69e09e5264eee
Brook Tekle (Opshadow) <126861167+Brooktek@users.noreply.github.com>
2024-12-27 16:28:57 +0300

    updated register

--- VOTE/VOTE/RegisterPage.xaml.cs
+++ VOTE/VOTE/RegisterPage.xaml.cs
@@ -1,27 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace VOTE
 {
-    /// <summary>
-    /// Interaction logic for RegisterPage.xaml
-    /// </summary>
     public partial class RegisterPage : Window
     {
+        // Keep track of the current page
+        private int currentPage = 0;
+
         public RegisterPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedRole = (RoleComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+            if (selectedRole == "Voter")
+            {
+                UserPanel.Visibility = Visibility.Hidden;
+                VoterPanel.Visibility = Visibility.Visible;
+            }
+            else if(selectedRole == "Party")
+            {
+                UserPanel.Visibility = Visibility.Hidden;
+                PartyPanel.Visibility = Visibility.Visible;
+            }
         }
     }
 }
